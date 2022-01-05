2f6ccf1448030969f24e374eca6fe72bc973f3a6
joaopaulopires <joaopaulopiresdev@gmail.com>
2022-01-05 13:32:51 -0300

    Fix debug setting and wsgi

--- gestao_usuario/wsgi.py
+++ gestao_usuario/wsgi.py
@@ -12,6 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestao_usuario.settings')
 
 application = Cling(get_wsgi_application())
